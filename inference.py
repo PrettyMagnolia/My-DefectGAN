e2a0c7cd41099ffe3bb8ef349aa29d5a9fb80ec0
Yifei Liu <3155002905@qq.com>
2024-03-13 21:23:12 +0800

    update: 修改显示函数逻辑，同时保存缺陷图片和正常图片

--- inference.py
+++ inference.py
@@ -1,14 +1,16 @@
+import os
 import numpy as np
 from matplotlib import pyplot as plt
 
 from utils_ import swap_axes
 
 
-def show_image(normal, top, m):
+def show_image(normal, top, m, step, dir_):
     top_layer = swap_axes(top.cpu().detach().numpy()[0])
     m = swap_axes(m.cpu().detach().numpy()[0])
 
     from_ = swap_axes(normal.cpu().detach().numpy()[0])
     gen_img = from_ * (1 - m) + top_layer * m
-    # plt.imshow(((gen_img + 1) * 127.5).astype(np.uint8))
-    plt.imsave("./official_visual.jpg", ((gen_img + 1) * 127.5).astype(np.uint8))
+
+    plt.imsave(os.path.join(dir_, f"defect_epoch{step + 1}.jpg"), ((gen_img + 1) * 127.5).astype(np.uint8))
+    plt.imsave(os.path.join(dir_, f"normal_epoch{step + 1}.jpg"), ((from_ + 1) * 127.5).astype(np.uint8))
